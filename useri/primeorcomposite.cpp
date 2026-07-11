1316d06b079a3502efb46d170b0e6a369bada7d0
Anushka Singh <anushka565dg@gmail.com>
2026-07-11 16:50:08 +0530

    Final contribution fix

---
+++ useri/primeorcomposite.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    bool flag=false;
+    for (int i=2; i<=n-1 ; i++){
+        if(n%i == 0){
+            flag=true;
+            break;
+        }
+    }
+    if(flag==true)cout<<"Composite Number";
+
+    
+    else cout<<"Prime Number";
+
+
+
+}
